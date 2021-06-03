8f7faa5ad6981c802c5fb553ccbeb035ef668492
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-06-03 13:40:34 -0300

    Initial config.py

---
+++ historic_legends/config.py
@@ -0,0 +1,16 @@
+match_data_columns = ["platformId",
+                      "gameId",
+                      "champion",
+                      "queue",
+                      "season",
+                      "timestamp",
+                      "role",
+                      "lane"
+                      ]
+
+
+SCHEMA_OUTPUT = "league_data"
+MATCHES_TABLE_OUTPUT = "matches_summary"
+TEST_MATCH_DATA = "test_matches_summary"
+
+REGION = "br1"
