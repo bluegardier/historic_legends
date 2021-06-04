5395179739f7ee362a0e57d4e643e5adb7839c25
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-06-04 19:18:07 -0300

    Removed unused import / index_list is now defaulted as None

--- src/domain/services.py
+++ src/domain/services.py
@@ -1,4 +1,3 @@
-from domain.repositories import MatchRepository
 from domain.repositories import Match, MatchRepository
 
 
@@ -6,7 +5,9 @@ class PopulateMatches:
     def __init__(self, repository: MatchRepository) -> None:
         self.repository = repository
 
-    def execute(self, index_list: list = [0]):
+    def execute(self, index_list=None):
 
+        if index_list is None:
+            index_list = [0]
         data = self.repository.fetch_data(index_list)
         self.repository.insert_match(data)
