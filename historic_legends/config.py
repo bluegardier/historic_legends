3792241778f089c3ebe67b889d198e410c483324
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-06-04 19:07:59 -0300

    removed TEST_MATCH_DATA / fixed MATCHES_TABLE_OUTPUT from "matches_summary" to "match_summary"

--- historic_legends/config.py
+++ historic_legends/config.py
@@ -10,7 +10,6 @@ match_data_columns = ["platformId",
 
 
 SCHEMA_OUTPUT = "league_data"
-MATCHES_TABLE_OUTPUT = "matches_summary"
-TEST_MATCH_DATA = "test_matches_summary"
+MATCHES_TABLE_OUTPUT = "match_summary"
 
 REGION = "br1"
