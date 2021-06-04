10b2cfe79370e316c2ab1a8352e1c9774d47351f
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-06-04 19:08:27 -0300

    main.py now uses CLI interaction by Fire module

--- src/main.py
+++ src/main.py
@@ -1,9 +1,37 @@
 from application.repositories import PostGresRiotMatchRepository
 from domain.services import PopulateMatches
-import sys
+import fire
 
-repo = PostGresRiotMatchRepository()
+BEGIN_INDEX = [0]
 
-service = PopulateMatches(repo)
-index_list = list(map(lambda x: int(x), sys.argv[1:]))
-service.execute(index_list)
+
+def upload_match_summary_data(begin_index=None):
+    """
+    Uploads the Match Summary table to PostGres Database.
+
+    Parameters
+    ----------
+    begin_index : Optional
+    List of int values. They get next 100 data matches from the
+    API starting from the begin_index element.
+
+    Returns
+    -------
+
+    """
+    if begin_index is None:
+        begin_index = BEGIN_INDEX
+
+    repo = PostGresRiotMatchRepository()
+    service = PopulateMatches(repo)
+    index_list = begin_index
+    service.execute(index_list)
+
+
+def cli():
+    """ Caller to transform module in a low-level CLI """
+    return fire.Fire()
+
+
+if __name__ == '__main__':
+    cli()
