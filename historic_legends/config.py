9eefbecdad2d95ce10b9af53b8e51b3b67349738
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-06-05 13:52:54 -0300

    Added PLAYER_NAME and match_victory_columns

--- historic_legends/config.py
+++ historic_legends/config.py
@@ -8,8 +8,20 @@ match_data_columns = ["platformId",
                       "lane"
                       ]
 
+match_victory_columns = [
+    "gameid",
+    "gamemode",
+    "gametype",
+    "teamid",
+    "win"
+]
+
 REGION = "br1"
+PLAYER_NAME = "Blue Gardier"
 BEGIN_INDEX = [0]
+
+
+DB_GAMEID_QUERY = "SELECT DISTINCT gameid FROM league_data.match_summary"
 
 SCHEMA_OUTPUT = "league_data"
 MATCHES_TABLE_OUTPUT = "match_summary"
