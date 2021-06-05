6a99dc8d73064525f09ce139c20d1d6b2ed712f0
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-06-05 13:53:53 -0300

    psycopg conn declared for database connection

--- src/infra/pandas_sql_connection.py
+++ src/infra/pandas_sql_connection.py
@@ -1,8 +1,9 @@
 import os
+import psycopg2
 import sqlalchemy as db
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
@@ -10,6 +11,11 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-
 engine = db.create_engine(
     f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+conn = psycopg2.connect(dbname=DB_NAME,
+                        user=DB_USER,
+                        password=DB_PASS,
+                        host=DB_HOST,
+                        port=DB_PORT)
