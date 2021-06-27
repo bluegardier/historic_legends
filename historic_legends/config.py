f73672c15fc00f961b3da3d0542ea88e76d874bc
Victor Dualibi <victor.dualibi@neoway.com.br>
2021-06-26 22:14:58 -0300

    now exports connection from infra

--- historic_legends/config.py
+++ historic_legends/config.py
@@ -1,3 +1,27 @@
+# TODO: Import conn from infra instead of config.py
+
+import os
+import psycopg2
+import sqlalchemy as db
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+engine = db.create_engine(
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+conn = psycopg2.connect(dbname=DB_NAME,
+                        user=DB_USER,
+                        password=DB_PASS,
+                        host=DB_HOST,
+                        port=DB_PORT)
+
 match_data_columns = ["platformId",
                       "gameId",
                       "champion",
@@ -20,8 +44,8 @@ REGION = "br1"
 PLAYER_NAME = "Blue Gardier"
 BEGIN_INDEX = [0]
 
-
-DB_GAMEID_QUERY = "SELECT DISTINCT gameid FROM league_data.match_summary"
+GAMEID_MATCH_SUMMARY_QUERY = "SELECT DISTINCT gameid from league_data.match_summary"
+GAMEID_TEAM_SUMMARY_QUERY = "SELECT DISTINCT gameid from league_data.match_team_status"
 
 SCHEMA_OUTPUT = "league_data"
 MATCHES_TABLE_OUTPUT = "match_summary"
